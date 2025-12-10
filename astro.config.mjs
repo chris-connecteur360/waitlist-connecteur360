b0220e9d1a72be516d1762c2f4c1ecce95a68e54
ChrisCoolDev <talchrist09@gmail.com>
2025-12-10 20:41:02 +0300

    set adapter to vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,13 +3,13 @@ import { defineConfig } from 'astro/config';
 
 import node from '@astrojs/node';
 
+import vercel from '@astrojs/vercel';
+
 // https://astro.build/config
 export default defineConfig({
   server: {
       host: true, // ou '0.0.0.0'
   },
   output: 'server',
-  adapter: node({
-    mode: 'standalone'
-  })
+  adapter: vercel()
 });
